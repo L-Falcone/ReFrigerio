49e24c202877b0bd36d621133d3a6dd9d1ece2f3
L-Falcone <134519277+L-Falcone@users.noreply.github.com>
2024-05-10 16:31:04 +0200

    ReFrigerio 1.0

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+document.getElementById('logo').addEventListener('click', function() {
+    this.classList.add('hidden');
+});
+
+document.getElementById('logo').addEventListener('click', function() {
+    document.body.style.overflow = 'visible'; 
+});
+
